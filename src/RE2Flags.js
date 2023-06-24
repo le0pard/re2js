8b676a7cfca512dc1063359afb3ac80063459df6
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-06-24 17:31:43 +0300

    add comments

--- src/RE2Flags.js
+++ src/RE2Flags.js
@@ -1,16 +1,42 @@
 class RE2Flags {
-  static FOLD_CASE = 1
-  static LITERAL = 2
-  static CLASS_NL = 4
-  static DOT_NL = 8
-  static ONE_LINE = 16
-  static NON_GREEDY = 32
-  static PERL_X = 64
-  static UNICODE_GROUPS = 128
-  static WAS_DOLLAR = 256
+  //// Parser flags.
+  // Fold case during matching (case-insensitive).
+  static FOLD_CASE = 0x01
+  // Treat pattern as a literal string instead of a regexp.
+  static LITERAL = 0x02
+  // Allow character classes like [^a-z] and [[:space:]] to match newline.
+  static CLASS_NL = 0x04
+  // Allow '.' to match newline.
+  static DOT_NL = 0x08
+  // Treat ^ and $ as only matching at beginning and end of text, not
+  // around embedded newlines.  (Perl's default).
+  static ONE_LINE = 0x10
+  // Make repetition operators default to non-greedy.
+  static NON_GREEDY = 0x20
+  // allow Perl extensions:
+  //   non-capturing parens - (?: )
+  //   non-greedy operators - *? +? ?? {}?
+  //   flag edits - (?i) (?-i) (?i: )
+  //     i - FoldCase
+  //     m - !OneLine
+  //     s - DotNL
+  //     U - NonGreedy
+  //   line ends: \A \z
+  //   \Q and \E to disable/enable metacharacters
+  //   (?P<name>expr) for named captures
+  // \C (any byte) is not supported.
+  static PERL_X = 0x40
+  // Allow \p{Han}, \P{Han} for Unicode group and negation.
+  static UNICODE_GROUPS = 0x80
+  // Regexp END_TEXT was $, not \z.  Internal use only.
+  static WAS_DOLLAR = 0x100
+
   static MATCH_NL = RE2Flags.CLASS_NL | RE2Flags.DOT_NL
+  // As close to Perl as possible.
   static PERL = RE2Flags.CLASS_NL | RE2Flags.ONE_LINE | RE2Flags.PERL_X | RE2Flags.UNICODE_GROUPS
+  // POSIX syntax.
   static POSIX = 0
+  //// Anchors
   static UNANCHORED = 0
   static ANCHOR_START = 1
   static ANCHOR_BOTH = 2
