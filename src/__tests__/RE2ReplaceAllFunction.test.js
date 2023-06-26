750ad1fc412e2c69cb0523f6e06bf2a3ae4e570d
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-06-26 22:53:54 +0300

    add test

---
+++ src/__tests__/RE2ReplaceAllFunction.test.js
@@ -0,0 +1,16 @@
+import { expect, describe, test } from '@jest/globals'
+import { RE2 } from '../RE2'
+
+describe('.replaceAllFunc', () => {
+  const cases = [
+    ['[a-c]', 'defabcdef', 'defxayxbyxcydef'],
+    ['[a-c]+', 'defabcdef', 'defxabcydef'],
+    ['[a-c]*', 'defabcdef', 'xydxyexyfxabcydxyexyfxy']
+  ]
+
+  test.each(cases)('pattern %p with input %p will return %p', (pattern, input, expected) => {
+    const replaceFunc = { replace: (s) => `x${s}y` }
+    const re = RE2.compile(pattern)
+    expect(re.replaceAllFunc(input, replaceFunc, input.length)).toEqual(expected)
+  })
+})
