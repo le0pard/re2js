490ca58712d16a267e958e89989a919b29adbfa3
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-07-09 20:27:32 +0300

    cleanup compiler

--- src/Compiler.js
+++ src/Compiler.js
@@ -1,21 +1,38 @@
-/* Generated from Java with JSweet 3.1.0 - http://www.jsweet.org */
+import { Codepoint } from './Codepoint'
 import { RE2Flags } from './RE2Flags'
 import { Unicode } from './Unicode'
 import { Utils } from './Utils'
 import { Regexp } from './Regexp'
 import { Inst } from './Inst'
 import { Prog } from './Prog'
+
+/**
+ * A fragment of a compiled regular expression program.
+ *
+ * @see http://swtch.com/~rsc/regexp/regexp1.html
+ * @class
+ */
+class Frag {
+  constructor(i = 0, out = 0, nullable = false) {
+    this.i = i // an instruction address (pc).
+    this.out = out // a patch list; see explanation in Prog.js
+    this.nullable = nullable // whether the fragment can match the empty string
+  }
+}
 /**
  * Compiler from {@code Regexp} (RE2 abstract syntax) to {@code RE2} (compiled regular expression).
  *
  * The only entry point is {@link #compileRegexp}.
- * @class
  */
 class Compiler {
-  constructor() {
-    this.prog = new Prog()
-    this.newInst(Inst.FAIL)
+  static ANY_RUNE_NOT_NL() {
+    return [0, Codepoint.CODES.get('\n') - 1, Codepoint.CODES.get('\n') + 1, Unicode.MAX_RUNE]
   }
+
+  static ANY_RUNE() {
+    return [0, Unicode.MAX_RUNE]
+  }
+
   static compileRegexp(re) {
     const c = new Compiler()
     const f = c.compile(re)
@@ -23,18 +40,30 @@ class Compiler {
     c.prog.start = f.i
     return c.prog
   }
+
+  constructor() {
+    this.prog = new Prog()
+    this.newInst(Inst.FAIL)
+  }
+
   newInst(op) {
     this.prog.addInst(op)
-    return new Compiler.Frag(this.prog.numInst() - 1, 0, true)
+    return new Frag(this.prog.numInst() - 1, 0, true)
   }
+
+  // Returns a no-op fragment.  Sometimes unavoidable.
   nop() {
     const f = this.newInst(Inst.NOP)
     f.out = f.i << 1
     return f
   }
+
   fail() {
-    return new Compiler.Frag()
+    return new Frag()
   }
+
+  // Given fragment a, returns (a) capturing as \n.
+  // Given a fragment a, returns a fragment with capturing parens around a.
   cap(arg) {
     const f = this.newInst(Inst.CAPTURE)
     f.out = f.i << 1
@@ -44,14 +73,21 @@ class Compiler {
     }
     return f
   }
+
+  // Given fragments a and b, returns ab; a|b
   cat(f1, f2) {
+    // concat of failure is failure
     if (f1.i === 0 || f2.i === 0) {
       return this.fail()
     }
+    // TODO(rsc): elide nop
     this.prog.patch(f1.out, f2.i)
-    return new Compiler.Frag(f1.i, f2.out, f1.nullable && f2.nullable)
+    return new Frag(f1.i, f2.out, f1.nullable && f2.nullable)
   }
+
+  // Given fragments for a and b, returns fragment for a|b.
   alt(f1, f2) {
+    // alt of failure is other
     if (f1.i === 0) {
       return f2
     }
@@ -66,6 +102,12 @@ class Compiler {
     f.nullable = f1.nullable || f2.nullable
     return f
   }
+
+  // loop returns the fragment for the main loop of a plus or star.
+  // For plus, it can be used directly. with f1.i as the entry.
+  // For star, it can be used directly when f1 can't match an empty string.
+  // (When f1 can match an empty string, f1* must be implemented as (f1+)?
+  // to get the priority match order correct.)
   loop(f1, nongreedy) {
     const f = this.newInst(Inst.ALT)
     const i = this.prog.getInst(f.i)
@@ -79,6 +121,8 @@ class Compiler {
     this.prog.patch(f1.out, f.i)
     return f
   }
+
+  // Given a fragment for a, returns a fragment for a? or a?? (if nongreedy)
   quest(f1, nongreedy) {
     const f = this.newInst(Inst.ALT)
     const i = this.prog.getInst(f.i)
@@ -92,25 +136,30 @@ class Compiler {
     f.out = this.prog.append(f.out, f1.out)
     return f
   }
+
+  // Given a fragment a, returns a fragment for a* or a*? (if nongreedy)
   star(f1, nongreedy) {
     if (f1.nullable) {
       return this.quest(this.plus(f1, nongreedy), nongreedy)
     }
     return this.loop(f1, nongreedy)
   }
+
+  // Given a fragment for a, returns a fragment for a+ or a+? (if nongreedy)
   plus(f1, nongreedy) {
-    return new Compiler.Frag(f1.i, this.loop(f1, nongreedy).out, f1.nullable)
+    return new Frag(f1.i, this.loop(f1, nongreedy).out, f1.nullable)
   }
+
+  // op is a bitmask of EMPTY_* flags.
   empty(op) {
     const f = this.newInst(Inst.EMPTY_WIDTH)
     this.prog.getInst(f.i).arg = op
     f.out = f.i << 1
     return f
   }
-  rune$int$int(rune, flags) {
-    return this.rune$int_A$int([rune], flags)
-  }
-  rune$int_A$int(runes, flags) {
+
+  // flags : parser flags
+  rune(runes, flags) {
     const f = this.newInst(Inst.RUNE)
     f.nullable = false
     const i = this.prog.getInst(f.i)
@@ -131,49 +180,15 @@ class Compiler {
     } else if (
       runes.length === 4 &&
       runes[0] === 0 &&
-      runes[1] === '\n'.codePointAt(0) - 1 &&
-      runes[2] === '\n'.codePointAt(0) + 1 &&
+      runes[1] === Codepoint.CODES.get('\n') - 1 &&
+      runes[2] === Codepoint.CODES.get('\n') + 1 &&
       runes[3] === Unicode.MAX_RUNE
     ) {
       i.op = Inst.RUNE_ANY_NOT_NL
     }
     return f
   }
-  rune(runes, flags) {
-    if (
-      ((runes != null &&
-        runes instanceof Array &&
-        (runes.length == 0 || runes[0] == null || typeof runes[0] === 'number')) ||
-        runes === null) &&
-      (typeof flags === 'number' || flags === null)
-    ) {
-      return this.rune$int_A$int(runes, flags)
-    } else if (
-      (typeof runes === 'number' || runes === null) &&
-      (typeof flags === 'number' || flags === null)
-    ) {
-      return this.rune$int$int(runes, flags)
-    } else {
-      throw new Error('invalid overload')
-    }
-  }
-  static ANY_RUNE_NOT_NL_$LI$() {
-    if (Compiler.ANY_RUNE_NOT_NL == null) {
-      Compiler.ANY_RUNE_NOT_NL = [
-        0,
-        '\n'.codePointAt(0) - 1,
-        '\n'.codePointAt(0) + 1,
-        Unicode.MAX_RUNE
-      ]
-    }
-    return Compiler.ANY_RUNE_NOT_NL
-  }
-  static ANY_RUNE_$LI$() {
-    if (Compiler.ANY_RUNE == null) {
-      Compiler.ANY_RUNE = [0, Unicode.MAX_RUNE]
-    }
-    return Compiler.ANY_RUNE
-  }
+
   compile(re) {
     switch (re.op) {
       case Regexp.Op.NO_MATCH:
@@ -188,18 +203,18 @@ class Compiler {
           for (let index = 0; index < re.runes.length; index++) {
             let r = re.runes[index]
             {
-              const f1 = this.rune$int$int(r, re.flags)
+              const f1 = this.rune([r], re.flags)
               f = f == null ? f1 : this.cat(f, f1)
             }
           }
           return f
         }
       case Regexp.Op.CHAR_CLASS:
-        return this.rune$int_A$int(re.runes, re.flags)
+        return this.rune(re.runes, re.flags)
       case Regexp.Op.ANY_CHAR_NOT_NL:
-        return this.rune$int_A$int(Compiler.ANY_RUNE_NOT_NL_$LI$(), 0)
+        return this.rune(Compiler.ANY_RUNE_NOT_NL(), 0)
       case Regexp.Op.ANY_CHAR:
-        return this.rune$int_A$int(Compiler.ANY_RUNE_$LI$(), 0)
+        return this.rune(Compiler.ANY_RUNE(), 0)
       case Regexp.Op.BEGIN_LINE:
         return this.empty(Utils.EMPTY_BEGIN_LINE)
       case Regexp.Op.END_LINE:
@@ -218,14 +233,13 @@ class Compiler {
         const ket = this.cap((re.cap << 1) | 1)
         return this.cat(this.cat(bra, sub), ket)
       }
-
       case Regexp.Op.STAR:
         return this.star(this.compile(re.subs[0]), (re.flags & RE2Flags.NON_GREEDY) !== 0)
       case Regexp.Op.PLUS:
         return this.plus(this.compile(re.subs[0]), (re.flags & RE2Flags.NON_GREEDY) !== 0)
       case Regexp.Op.QUEST:
         return this.quest(this.compile(re.subs[0]), (re.flags & RE2Flags.NON_GREEDY) !== 0)
-      case Regexp.Op.CONCAT:
+      case Regexp.Op.CONCAT: {
         if (re.subs.length === 0) {
           return this.nop()
         } else {
@@ -236,6 +250,7 @@ class Compiler {
           }
           return f
         }
+      }
       case Regexp.Op.ALTERNATE: {
         if (re.subs.length === 0) {
           return this.nop()
@@ -248,172 +263,10 @@ class Compiler {
           return f
         }
       }
-
       default:
-        throw Object.defineProperty(new Error('regexp: unhandled case in compile'), '__classes', {
-          configurable: true,
-          value: [
-            'java.lang.Throwable',
-            'java.lang.IllegalStateException',
-            'java.lang.Object',
-            'java.lang.RuntimeException',
-            'java.lang.Exception'
-          ]
-        })
+        throw new Error('regexp: unhandled case in compile')
     }
   }
 }
-Compiler['__class'] = 'quickstart.Compiler'
-;(function (Compiler) {
-  /**
-   * A fragment of a compiled regular expression program.
-   *
-   * @see http://swtch.com/~rsc/regexp/regexp1.html
-   * @class
-   */
-  class Frag {
-    constructor(i, out, nullable) {
-      if (
-        (typeof i === 'number' || i === null) &&
-        (typeof out === 'number' || out === null) &&
-        (typeof nullable === 'boolean' || nullable === null)
-      ) {
-        let __args = arguments
-        if (this.i === undefined) {
-          this.i = 0
-        }
-        if (this.out === undefined) {
-          this.out = 0
-        }
-        if (this.nullable === undefined) {
-          this.nullable = false
-        }
-        this.i = i
-        this.out = out
-        this.nullable = nullable
-      } else if (
-        (typeof i === 'number' || i === null) &&
-        (typeof out === 'number' || out === null) &&
-        nullable === undefined
-      ) {
-        let __args = arguments
-        {
-          let __args = arguments
-          let nullable = false
-          if (this.i === undefined) {
-            this.i = 0
-          }
-          if (this.out === undefined) {
-            this.out = 0
-          }
-          if (this.nullable === undefined) {
-            this.nullable = false
-          }
-          this.i = i
-          this.out = out
-          this.nullable = nullable
-        }
-        if (this.i === undefined) {
-          this.i = 0
-        }
-        if (this.out === undefined) {
-          this.out = 0
-        }
-        if (this.nullable === undefined) {
-          this.nullable = false
-        }
-      } else if (
-        (typeof i === 'number' || i === null) &&
-        out === undefined &&
-        nullable === undefined
-      ) {
-        let __args = arguments
-        {
-          let __args = arguments
-          let out = 0
-          {
-            let __args = arguments
-            let nullable = false
-            if (this.i === undefined) {
-              this.i = 0
-            }
-            if (this.out === undefined) {
-              this.out = 0
-            }
-            if (this.nullable === undefined) {
-              this.nullable = false
-            }
-            this.i = i
-            this.out = out
-            this.nullable = nullable
-          }
-          if (this.i === undefined) {
-            this.i = 0
-          }
-          if (this.out === undefined) {
-            this.out = 0
-          }
-          if (this.nullable === undefined) {
-            this.nullable = false
-          }
-        }
-        if (this.i === undefined) {
-          this.i = 0
-        }
-        if (this.out === undefined) {
-          this.out = 0
-        }
-        if (this.nullable === undefined) {
-          this.nullable = false
-        }
-      } else if (i === undefined && out === undefined && nullable === undefined) {
-        let __args = arguments
-        {
-          let __args = arguments
-          let i = 0
-          let out = 0
-          {
-            let __args = arguments
-            let nullable = false
-            if (this.i === undefined) {
-              this.i = 0
-            }
-            if (this.out === undefined) {
-              this.out = 0
-            }
-            if (this.nullable === undefined) {
-              this.nullable = false
-            }
-            this.i = i
-            this.out = out
-            this.nullable = nullable
-          }
-          if (this.i === undefined) {
-            this.i = 0
-          }
-          if (this.out === undefined) {
-            this.out = 0
-          }
-          if (this.nullable === undefined) {
-            this.nullable = false
-          }
-        }
-        if (this.i === undefined) {
-          this.i = 0
-        }
-        if (this.out === undefined) {
-          this.out = 0
-        }
-        if (this.nullable === undefined) {
-          this.nullable = false
-        }
-      } else {
-        throw new Error('invalid overload')
-      }
-    }
-  }
-  Compiler.Frag = Frag
-  Frag['__class'] = 'quickstart.Compiler.Frag'
-})(Compiler || (Compiler = {}))
 
 export { Compiler }
