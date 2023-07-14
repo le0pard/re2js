c29e0d0ec211dfc487f7fb2a099c81b7dca33953
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-07-14 15:55:36 +0300

    cleanup pattern

---
+++ src/RE2JSSyntaxException.js
@@ -0,0 +1,32 @@
+/**
+ * An exception thrown by the parser if the pattern was invalid.
+ */
+class RE2JSSyntaxException extends Error {
+  constructor(error, input = null) {
+    let message = `error parsing regexp: ${error}`
+    if (input) {
+      message += `: \`${input}\``
+    }
+
+    super(message)
+    this.message = message
+    this.error = error
+    this.input = input
+  }
+
+  /**
+   * Retrieves the description of the error.
+   */
+  getDescription() {
+    return this.error
+  }
+
+  /**
+   * Retrieves the erroneous regular-expression pattern.
+   */
+  getPattern() {
+    return this.input
+  }
+}
+
+export { RE2JSSyntaxException }
