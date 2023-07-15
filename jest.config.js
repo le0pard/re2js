f5938b149e2b99ac0dfb8f13e042382e9841d52a
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-07-15 19:25:38 +0300

    add clearMocks

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ process.env.TZ = 'UTC' // normalize timezone for tests
 export default { // eslint-disable-line import/no-default-export
   transform: {},
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  clearMocks: true
 }
