83dbd27d4a5c42d525d25046afc9ee9662b483e0
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-09-03 21:26:49 +0300

    add more tests

--- src/__tests__/Unicode.test.js
+++ src/__tests__/Unicode.test.js
@@ -2,6 +2,47 @@ import { Unicode } from '../Unicode'
 import { codePoint } from '../__utils__/chars'
 import { expect, describe, test } from '@jest/globals'
 
+describe('#isUpper', () => {
+  test.concurrent.each([
+    [115, false], // 's'
+    [83, true], // 'S'
+    [503, true], // 'Ƿ'
+    [469, true], // 'Ǖ'
+    [474, false], // 'ǚ'
+    [940, false] // 'ά'
+  ])(
+    '#isUpper(%p) === %p',
+    (input, expected) => {
+      expect(Unicode.isUpper(input)).toEqual(expected)
+    }
+  )
+})
+
+describe('#simpleFold', () => {
+  test.concurrent.each([
+    // 'A' <-> 'a'
+    [65, 97],
+    [97, 65],
+    // 's' <-> 'ſ' <-> 'S'
+    [83, 115],
+    [115, 383],
+    [383, 83],
+    // 'K' <-> '\u212A' (Kelvin symbol, K) <-> 'k'
+    [75, 107],
+    [107, 8490],
+    [8490, 75],
+    // '1'
+    [49, 49],
+    // '9'
+    [57, 57]
+  ])(
+    '#simpleFold(%p) === %p',
+    (input, expected) => {
+      expect(Unicode.simpleFold(input)).toEqual(expected)
+    }
+  )
+})
+
 const genEqualsIgnoreCases = () => {
   let testCases = [
     [codePoint('{'), codePoint('{'), true],
