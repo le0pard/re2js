d90e1eed021442c7153bd3fd21aa7db657e637f1
Alexey Vasiliev <leopard.not.a@gmail.com>
2023-09-13 12:18:11 +0300

    add node.js in browserlist

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,13 @@ export default {
     [
       '@babel/preset-env',
       {
-        targets: ['>0.5%', 'Firefox ESR', 'not dead', 'not op_mini all'],
+        targets: [
+          '>0.3%',
+          'Firefox ESR',
+          'not dead',
+          'not op_mini all',
+          'maintained node versions'
+        ],
         modules: 'auto',
         useBuiltIns: false
       }
