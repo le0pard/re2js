6fe44bd68f101d6993de9a60eb220ae04f334fbd
Alexey Vasiliev <leopard.not.a@gmail.com>
2024-03-01 03:36:25 +0200

    remove jsdom - not needed

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,5 @@ process.env.TZ = 'UTC' // normalize timezone for tests
 export default {
   transform: {},
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
-  testEnvironment: 'jsdom',
   clearMocks: true
 }
