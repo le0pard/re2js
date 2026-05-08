7f27daf80132b0bb05673f5891d027f13b7bca36
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-05-08 23:50:13 +0300

    fix ci scripts

---
+++ jest.deno.config.js
@@ -0,0 +1,8 @@
+process.env.TZ = 'UTC' // normalize timezone for tests
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
+  clearMocks: true,
+  restoreMocks: true
+}
