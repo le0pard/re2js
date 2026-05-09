a0591d649d5b848c5e71beda528ae334debbde40
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-05-09 18:53:40 +0300

    Migrate to vitest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-process.env.TZ = 'UTC' // normalize timezone for tests
-
-// eslint-disable-next-line import/no-default-export
-export default {
-  transform: {},
-  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
-  clearMocks: true,
-  restoreMocks: true
-}
