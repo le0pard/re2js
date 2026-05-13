3b44afb96d35f8c60c32278cdeda63338dc585e0
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-05-14 01:07:09 +0300

    add more tests

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -821,6 +821,155 @@ describe('replaceAll and replaceFirst', () => {
   })
 })
 
+describe('replaceAll and replaceFirst using replacer function', () => {
+  const cases = [
+    // Test empty input and/or replacement,
+    // with pattern that matches the empty string.
+    ['', '', () => '', '', false],
+    ['', '', () => 'x', 'x', false],
+    ['', 'abc', () => '', 'abc', false],
+    ['', 'abc', () => 'x', 'xaxbxcx', false],
+
+    // Test empty input and/or replacement,
+    // with pattern that does not match the empty string.
+    ['b', '', () => '', '', false],
+    ['b', '', () => 'x', '', false],
+    ['b', 'abc', () => '', 'ac', false],
+    ['b', 'abc', () => 'x', 'axc', false],
+    ['y', '', () => '', '', false],
+    ['y', '', () => 'x', '', false],
+    ['y', 'abc', () => '', 'abc', false],
+    ['y', 'abc', () => 'x', 'abc', false],
+
+    // Multibyte characters
+    ['[a-c]*', '\u65e5', () => 'x', 'x\u65e5x', false],
+    ['[^\u65e5]', 'abc\u65e5def', () => 'x', 'xxx\u65e5xxx', false],
+
+    ['a{2,10}', 'aabaaa', () => 'x', 'xbx', false],
+    ['a{5,10}', 'aabaaaaa', () => 'x', 'aabx', false],
+
+    // Start and end of a string.
+    ['^[a-c]*', 'abcdabc', () => 'x', 'xdabc', false],
+    ['[a-c]*$', 'abcdabc', () => 'x', 'abcdxx', false],
+    ['^[a-c]*$', 'abcdabc', () => 'x', 'abcdabc', false],
+    ['^[a-c]*', 'abc', () => 'x', 'x', false],
+    ['[a-c]*$', 'abc', () => 'x', 'xx', false],
+    ['^[a-c]*$', 'abc', () => 'x', 'x', false],
+    ['^[a-c]*', 'dabce', () => 'x', 'xdabce', false],
+    ['[a-c]*$', 'dabce', () => 'x', 'dabcex', false],
+    ['^[a-c]*$', 'dabce', () => 'x', 'dabce', false],
+    ['^[a-c]*', '', () => 'x', 'x', false],
+    ['[a-c]*$', '', () => 'x', 'x', false],
+    ['^[a-c]*$', '', () => 'x', 'x', false],
+    ['^[a-c]+', 'abcdabc', () => 'x', 'xdabc', false],
+    ['[a-c]+$', 'abcdabc', () => 'x', 'abcdx', false],
+    ['^[a-c]+$', 'abcdabc', () => 'x', 'abcdabc', false],
+    ['^[a-c]+', 'abc', () => 'x', 'x', false],
+    ['[a-c]+$', 'abc', () => 'x', 'x', false],
+    ['^[a-c]+$', 'abc', () => 'x', 'x', false],
+    ['^[a-c]+', 'dabce', () => 'x', 'dabce', false],
+    ['[a-c]+$', 'dabce', () => 'x', 'dabce', false],
+    ['^[a-c]+$', 'dabce', () => 'x', 'dabce', false],
+    ['^[a-c]+', '', () => 'x', '', false],
+    ['[a-c]+$', '', () => 'x', '', false],
+    ['^[a-c]+$', '', () => 'x', '', false],
+
+    // Other cases.
+    ['abc', 'abcdefg', () => 'def', 'defdefg', false],
+    ['bc', 'abcbcdcdedef', () => 'BC', 'aBCBCdcdedef', false],
+    ['abc', 'abcdabc', () => '', 'd', false],
+    ['x', 'xxxXxxx', () => 'xXx', 'xXxxXxxXxXxXxxXxxXx', false],
+    ['abc', '', () => 'd', '', false],
+    ['abc', 'abc', () => 'd', 'd', false],
+    ['.+', 'abc', () => 'x', 'x', false],
+    ['[a-c]*', 'def', () => 'x', 'xdxexfx', false],
+    ['[a-c]+', 'abcbcdcdedef', () => 'x', 'xdxdedef', false],
+    ['[a-c]*', 'abcbcdcdedef', () => 'x', 'xxdxxdxexdxexfx', false],
+
+    // Test empty input and/or replacement,
+    // with pattern that matches the empty string.
+    ['', '', () => '', '', true],
+    ['', '', () => 'x', 'x', true],
+    ['', 'abc', () => '', 'abc', true],
+    ['', 'abc', () => 'x', 'xabc', true],
+
+    // Test empty input and/or replacement,
+    // with pattern that does not match the empty string.
+    ['b', '', () => '', '', true],
+    ['b', '', () => 'x', '', true],
+    ['b', 'abc', () => '', 'ac', true],
+    ['b', 'abc', () => 'x', 'axc', true],
+    ['y', '', () => '', '', true],
+    ['y', '', () => 'x', '', true],
+    ['y', 'abc', () => '', 'abc', true],
+    ['y', 'abc', () => 'x', 'abc', true],
+
+    // Multibyte characters
+    ['[a-c]*', '\u65e5', () => 'x', 'x\u65e5', true],
+    ['[^\u65e5]', 'abc\u65e5def', () => 'x', 'xbc\u65e5def', true],
+
+    // Start and end of a string.
+    ['^[a-c]*', 'abcdabc', () => 'x', 'xdabc', true],
+    ['[a-c]*$', 'abcdabc', () => 'x', 'abcdx', true],
+    ['^[a-c]*$', 'abcdabc', () => 'x', 'abcdabc', true],
+    ['^[a-c]*', 'abc', () => 'x', 'x', true],
+    ['[a-c]*$', 'abc', () => 'x', 'x', true],
+    ['^[a-c]*$', 'abc', () => 'x', 'x', true],
+    ['^[a-c]*', 'dabce', () => 'x', 'xdabce', true],
+    ['[a-c]*$', 'dabce', () => 'x', 'dabcex', true],
+    ['^[a-c]*$', 'dabce', () => 'x', 'dabce', true],
+    ['^[a-c]*', '', () => 'x', 'x', true],
+    ['[a-c]*$', '', () => 'x', 'x', true],
+    ['^[a-c]*$', '', () => 'x', 'x', true],
+    ['^[a-c]+', 'abcdabc', () => 'x', 'xdabc', true],
+    ['[a-c]+$', 'abcdabc', () => 'x', 'abcdx', true],
+    ['^[a-c]+$', 'abcdabc', () => 'x', 'abcdabc', true],
+    ['^[a-c]+', 'abc', () => 'x', 'x', true],
+    ['[a-c]+$', 'abc', () => 'x', 'x', true],
+    ['^[a-c]+$', 'abc', () => 'x', 'x', true],
+    ['^[a-c]+', 'dabce', () => 'x', 'dabce', true],
+    ['[a-c]+$', 'dabce', () => 'x', 'dabce', true],
+    ['^[a-c]+$', 'dabce', () => 'x', 'dabce', true],
+    ['^[a-c]+', '', () => 'x', '', true],
+    ['[a-c]+$', '', () => 'x', '', true],
+    ['^[a-c]+$', '', () => 'x', '', true],
+
+    // Other cases.
+    ['abc', 'abcdefg', () => 'def', 'defdefg', true],
+    ['bc', 'abcbcdcdedef', () => 'BC', 'aBCbcdcdedef', true],
+    ['abc', 'abcdabc', () => '', 'dabc', true],
+    ['x', 'xxxXxxx', () => 'xXx', 'xXxxxXxxx', true],
+    ['abc', '', () => 'd', '', true],
+    ['abc', 'abc', () => 'd', 'd', true],
+    ['.+', 'abc', () => 'x', 'x', true],
+    ['[a-c]*', 'def', () => 'x', 'xdef', true],
+    ['[a-c]+', 'abcbcdcdedef', () => 'x', 'xdcdedef', true],
+    ['[a-c]*', 'abcbcdcdedef', () => 'x', 'xdcdedef', true],
+
+    // --- Dynamic Match / Capture Group Cases ---
+
+    // Replace dynamically using match
+    ['[a-c]+', 'defabcdef', (m) => m.toUpperCase(), 'defABCdef', false],
+    ['[a-c]+', 'abcbcdcdedef', (m) => m.toUpperCase(), 'ABCBCdcdedef', true],
+
+    // Replace dynamically using capture groups (p1, p2)
+    ['(\\w+) (\\w+)', 'hello world', (m, p1, p2) => `${p2} ${p1}`, 'world hello', false],
+    ['(a)(b)', 'xabx', (m, p1, p2) => p2 + p1, 'xbax', false],
+
+    // Utilizing offset and unmodified string references
+    ['x', 'axbxc', (m, offset) => offset.toString(), 'a1b3c', false]
+  ]
+
+  it.each(cases)(
+    'pattern %p with input %p and replacer fn will return %p (only first: %p)',
+    (pattern, input, replacerFn, expected, replaceFirst) => {
+      const re2 = RE2JS.compile(pattern)
+      const m = re2.matcher(input)
+      expect(replaceFirst ? m.replaceFirst(replacerFn) : m.replaceAll(replacerFn)).toEqual(expected)
+    }
+  )
+})
+
 it('does not re-evaluate characters inside named groups in JS mode', () => {
   const re2 = RE2JS.compile('(world)')
   const m = re2.matcher('hello world')
