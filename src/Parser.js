b57c211edb96111cc7ca175e9e270c13dae191da
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-07-30 00:52:03 +0300

    add correct comment

--- src/Parser.js
+++ src/Parser.js
@@ -448,7 +448,7 @@ class Parser {
           break
         }
       }
-      // oxlint-disable-next-line no-fallthrough
+      // fallsthrough
       case Codepoint.CODES.get('0'): {
         // Consume up to three octal digits; already have one.
         let r = c - Codepoint.CODES.get('0')
